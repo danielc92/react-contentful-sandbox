3fb9e700f396f0bf817764c86648541fb3fa0c85
Daniel Corcoran <danielc.92@outlook.com>
2020-09-05 17:32:55 +1000

    Added App.tsx content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { createClient, EntryCollection } from "contentful";
+import {
+  IHomePageBannerFields,
+} from "./schema/generated/contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { MARKS, BLOCKS } from '@contentful/rich-text-types'
+const client = createClient({
+  accessToken: process.env.REACT_APP_CONTENTFUL_DELIVERY_API_ACCESS_TOKEN || "",
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID || "",
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [results, setResults] = useState<null | EntryCollection<
+    IHomePageBannerFields
+  >>(null);
+
+  useEffect(() => {
+    client
+      .getEntries<IHomePageBannerFields>()
+      .then((results) => {
+        setResults(results);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (results && results.items.length > 0) {
+    const { fields } = results.items[0];
+    const { bannerRichText } = fields;
+    if (bannerRichText)
+      return (
+        <div style={{ maxWidth: "800px", margin: "0 auto" }}>
+          <h1>{fields.bannerTitle}</h1>
+          <img src={fields.bannerImage?.fields.file.url} alt="dog staring at you" />
+          {documentToReactComponents(bannerRichText)}
+        </div>
+      );
+  }
+
+  return <div>loading</div>;
 }
 
 export default App;
